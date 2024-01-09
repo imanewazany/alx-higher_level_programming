872daf199b46350b29acb0ed2812410a5544fdce
imanewazany <imane.elouazzany@gmail.com>
2024-01-08 16:13:16 -0800

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    total = len(sentence)
-    char = sentence[0] if total > 0 else "None"
-    new = total, char
-    return(new)
+    if len(sentence) == 0:
+        return(0, None)
+    return(len(sentence), sentence[0])
+
