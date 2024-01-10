acb4604529306944f937efe2f64caca693e13e9b
imanewazany <imane.elouazzany@gmail.com>
2024-01-10 13:55:19 -0800

    3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_l =[]
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_l.append(replace)
-        else:
-            new_l.append(my_list[i])
-    return new_l
+    response = [num if num != search else replace for num in my_list]
+    return response
